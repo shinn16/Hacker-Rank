8174adb46d83ef638f6d5cee7fdd890b8e1d5a5f
Patrick Shinn <shinn16@marshall.edu>
2017-04-02 01:17:21 -0400

    GradingStudents final

--- src/Algorithms/Implementation/GradingStudents/Solution.java
+++ src/Algorithms/Implementation/GradingStudents/Solution.java
@@ -1,5 +1,8 @@
 package Algorithms.Implementation.GradingStudents;
 
+import java.io.File;
+import java.util.Scanner;
+
 /**
  * Solution class
  *
@@ -7,4 +10,21 @@ package Algorithms.Implementation.GradingStudents;
  * @version 4/2/17
  */
 public class Solution {
+    public static void main(String[] args){
+        try{
+            Scanner scanner = new Scanner(System.in);
+            int size = scanner.nextInt();
+            for (int i = 0; i < size; i ++){
+                int grade = scanner.nextInt();
+                if (grade >= 38){
+                    if (grade % 5 >= 3){
+                        grade += 5 - (grade % 5);
+                    }
+                }
+                System.out.println(grade);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
