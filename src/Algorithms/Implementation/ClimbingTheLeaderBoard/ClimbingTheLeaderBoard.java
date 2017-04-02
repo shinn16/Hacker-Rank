3a75163636847a5ce69c94c4a35f9589ca85a579
Patrick Shinn <shinn16@marshall.edu>
2017-04-01 21:43:19 -0400

    Final solution

--- src/Algorithms/Implementation/ClimbingTheLeaderBoard/ClimbingTheLeaderBoard.java
+++ src/Algorithms/Implementation/ClimbingTheLeaderBoard/ClimbingTheLeaderBoard.java
@@ -1,56 +1,52 @@
 package Algorithms.Implementation.ClimbingTheLeaderBoard;
 
-import java.io.File;
 import java.util.*;
 
-/**
- * ClimbingTheLeaderBoard class
- *
- * @author Patrick Shinn
- * @version 4/1/17
- */
+
 public class ClimbingTheLeaderBoard {
+
     public static void main(String[] args) {
-        ArrayList<Integer> orderedScores = new ArrayList<>();
-        boolean scored = false;
-        HashSet<Integer> sort = new HashSet<>();
-        try{
-            Scanner in = new Scanner(System.in);
-
-            // put each scores in a heap to sort it.
-            int n = in.nextInt();
-            for(int scores_i=0; scores_i < n; scores_i++){
-                int value = in.nextInt();
-                if (sort.add(value)){ // remove any duplicates before inserting
-                    orderedScores.add(-1 * value);
+        Scanner in = new Scanner(System.in);
+        int n = in .nextInt();
+        int[] scores = new int[n];
+        int[] positions = new int[n];
+        for (int scores_i = 0; scores_i < n; scores_i++) {
+            scores[scores_i] = in .nextInt();
+            if (scores_i == 0) {
+                positions[0] = 1;
+            } else {
+                if (scores[scores_i] == scores[scores_i - 1]) {
+                    positions[scores_i] = positions[scores_i - 1];
+                } else {
+                    positions[scores_i] = positions[scores_i - 1] + 1;
                 }
             }
-            // sort
-            Collections.sort(orderedScores);
-            // building alice's scores
-            int m = in.nextInt();
-            int[] alice = new int[m];
-            for(int alice_i=0; alice_i < m; alice_i++){
-                alice[alice_i] = in.nextInt();
-            }
+        }
 
-            // comparing alice to other scores and printing rank
-            int rank = 1; // descending counter
-            for (int score : alice){ // for all of her scores
-                for (int otherScore: orderedScores){ // compare them to all the other score
-                    if (score >= -1 * otherScore){ // if she has one that is equal, give her the rank
-                        System.out.println(rank);
-                        scored = true; // let the next loop know she received a score.
-                        break;
-                    }
-                    rank ++; // otherwise, increment the rank.
+
+        int m = in .nextInt();
+        int[] alice = new int[m];
+        for (int alice_i = 0; alice_i < m; alice_i++) {
+            alice[m - alice_i - 1] = in .nextInt();
+        }
+
+        // Now this is where the fun begins
+        int i = 0;
+        int[] alice_position = new int[m];
+        for (int alice_i = 0; alice_i < m; alice_i++) {
+            for (; i < n; i++) {
+                if (alice[alice_i] >= scores[i]) {
+                    alice_position[alice_i] = positions[i];
+                    break;
                 }
-                if (!scored) System.out.println(rank); // if she came in last, print her score.
-                scored = false; // reset scored
-                rank = 1; // reset counter
             }
-        }catch (Exception e){
-            e.printStackTrace();
+            if (scores[n - 1] > alice[alice_i]) {
+                alice_position[alice_i] = positions[n - 1] + 1;
+            }
+        }
+
+        for (int alice_i = m - 1; alice_i >= 0; alice_i--) {
+            System.out.println(alice_position[alice_i]);
         }
 
     }
